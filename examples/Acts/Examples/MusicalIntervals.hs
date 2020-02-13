6d670a619dc9e9bbb24838d892b78e914fcd56c0
sheaf <sam.derbyshire@gmail.com>
2020-02-13 11:12:33 +0100

    acts, groups and torsors

---
+++ examples/Acts/Examples/MusicalIntervals.hs
@@ -0,0 +1,332 @@
+{-# LANGUAGE
+    DataKinds
+  , DeriveGeneric
+  , DerivingVia
+  , MultiParamTypeClasses
+  , PatternSynonyms
+  , TupleSections
+  , TypeApplications
+  , ViewPatterns
+#-}
+
+{-# OPTIONS_HADDOCK ignore-exports #-}
+
+{-|
+Module: Acts.MusicalIntervals
+
+Illustrative usage of 'Group', 'Act' and 'Torsor': manipulation of musical intervals.
+
+
+The musical distance between two musical notes is a musical interval.
+
+Intervals can be compounded and inverted, so they form a 'Group'.
+
+Notes can be translated by a given interval, which is an 'Act' of intervals on notes.
+
+There's a unique musical interval taking any note to any other given one, so notes are a torsor under intervals.
+
+
+This functionality is useful in providing enharmonically correct voicings of chords.
+-}
+
+module Acts.Examples.MusicalIntervals where
+
+-- base
+import Data.Monoid
+  ( Sum(..) )
+
+-- acts
+import Data.Act
+import Data.Group
+import Data.Group.Cyclic
+
+-----------------------------------------------------------------
+-- * Musical notes
+--
+-- $notenames
+-- We begin by defining note names, which are acted upon by the cyclic group of order 7.
+
+-- | Musical note names.
+--
+-- The enumeration starts with @C@ to conform with scientific pitch notation.
+data NoteName = C | D | E | F | G | A | B
+  deriving stock ( Eq, Ord, Show, Enum, Bounded )
+  deriving ( Act ( C 7 ), Torsor ( C 7 ) )
+    via CyclicEnum NoteName
+
+-- $deriving1
+-- In this case we used @DerivingVia@ to derive the action of @C 7@,
+-- using the 'CyclicEnum' newtype created for this exact purpose.
+
+-- | Alterations, i.e. sharps and flats.
+--
+-- Pattern synonyms such as 'Sharp' and 'Flat' are also bundled.
+newtype Alteration = Alteration { getAlteration :: Int }
+  deriving ( Semigroup, Monoid, Group )
+    via Sum Int
+
+-- $deriving2
+-- Note the use of @DerivingVia@ to transfer algebraic operations from @Sum Int@.
+-- 
+-- For non-newtypes, one can use generics, for example:
+--
+-- > data Klein4 = Klein4 ( C 2 ) ( C 2 )
+-- >   deriving stock Generic
+-- >   deriving ( Semigroup, Monoid, Group )
+-- >     via Generically Klein4
+--
+-- This uses the 'Generically' newtype from the @generic-data@ library.
+
+-- | Note names such as @A4@ or @C#6@: note name, alteration, and octave (scientific pitch notation).
+data Note = Note { name :: NoteName, alteration :: Alteration, octave :: Int }
+
+-----------------------------------------------------------------
+-- * Musical intervals
+--
+-- $intervals
+-- An interval is represented as a number of scale steps to take (relative to the major scale),
+-- together with an additional alteration to apply.
+--
+-- For instance, a major third is two steps up (diatonic steps relative to the root in a major scale):
+--
+-- > > Steps ( Sum 2 ) Natural
+-- > major 3rd up
+--
+-- A minor sixth is 5 steps up, and then a flat:
+--
+-- > > Steps ( Sum 5 ) Flat
+-- > minor 6th up
+--
+-- The smart constructor 'Interval' is also provided that is more intuitive to use:
+--
+-- > > Interval 3 Natural
+-- > major 3rd up
+--
+-- > > Interval 7 Flat
+-- > minor 7th up
+--
+-- Note that the @Semigroup@/@Group@ operations on intervals are __not__ the obvious ones, e.g.:
+--
+-- > > Steps ( Sum 2 ) Natural
+-- > major 3rd up
+--
+-- > > Steps ( Sum (-2) ) Natural
+-- > minor 3rd down
+--
+-- > > inverse ( Steps ( Sum 2 ) Natural )
+-- > Steps ( Sum (-2) ) Flat
+-- > major 3rd down
+
+-- | Musical interval: steps (relative to the root in a major scale) and additional alteration.
+data Interval = Steps { intervalSteps :: Sum Int, intervalAlteration :: Alteration }
+
+-- | Compute the number of semitones in an interval, using the reference of the C major scale.
+semitones :: Interval -> Int
+semitones ival = case act ival ( Note C Natural 0 ) of
+  Note n a o -> 12 * o + getAlteration a + majorValue
+    where
+      majorValue = let i = fromEnum n in 2 * i - fromEnum ( i >= 3 )
+
+-- $interval_operations
+-- To define algebraic operations on intervals,
+-- we use an equivariant bijection to the product group @( Sum Int, Sum Int )@.
+--
+-- Note that @( Sum Int, Sum Int )@ is automatically a 'Semigroup', 'Monoid' and 'Group'
+-- using the product structure.
+
+-- | Forward part of the bijection.
+straighten :: Interval -> ( Sum Int, Sum Int )
+straighten ival@( Steps steps _ ) = ( steps, Sum $ semitones ival )
+-- | Back part of the bijection.
+twist :: ( Sum Int, Sum Int ) -> Interval
+twist ( i, Sum a ) = Steps i ( Alteration ( semitones ( Steps i mempty ) ) --> Alteration a )
+
+instance Semigroup Interval where
+  iv1 <> iv2 = twist ( straighten iv1 <> straighten iv2 )
+instance Monoid Interval where
+  mempty = Steps mempty mempty
+instance Group Interval where
+  inverse = twist . inverse . straighten
+
+-- | Intervallically correct action of intervals on notes.
+--
+--  * minor third up from @C@: @Eb@
+--  * minor third up from @A@: @C@.
+instance Act Interval Note where
+  act ( Steps ( Sum steps ) a ) ( Note C a' o ) = Note ( act ( Cyclic @7 r ) C ) ( a <> a' ) ( q + o )
+    where
+      ( q, r ) = steps `divMod` 7
+  act ival note = act ( ival <> ( Note C Natural 0 --> note ) ) ( Note C Natural 0 )
+
+-- | Computes the interval between two notes.
+--
+-- > > Note C Natural 5 --> Note A Natural 4
+-- > minor 3rd down
+--
+-- > > Note E Flat 4 --> Note A Natural 5
+-- > augmented 11th up
+instance Torsor Interval Note where
+  Note C a o --> Note n a' o' = Steps ( Sum $ fromEnum n + 7 * (o' - o) ) ( a --> a' )
+  note1 --> note2 = ( Note C Natural 0 --> note1 :: Interval ) --> ( Note C Natural 0 --> note2 :: Interval )
+
+-----------------------------------------------------------------
+-- * Illustration of the functionality
+
+-- ** Chords
+
+-- | Major triad: major third, perfect fifth.
+majorTriad :: [ Interval ]
+majorTriad = [ mempty, Interval 3 Natural, Interval 5 Natural ]
+
+-- | Diminished seventh chord: minor third, diminished fifth, diminished seventh.
+diminished7th :: [ Interval ]
+diminished7th = [ mempty, Interval 3 Flat, Interval 5 Flat, Interval 7 DoubleFlat ]
+
+-- | Minor 11th chord (Kenny Barron voicing).
+minor11th :: [ Interval ]
+minor11th = [ mempty, Interval 5 Natural, Interval 9 Natural
+            , Interval 10 Flat, Interval 14 Flat, Interval 18 Natural
+            ]
+
+-- $chords
+-- Example chords:
+--
+-- > > majorTriad <&> ( • Note C Natural 4 )
+-- > [C4,E4,G4]
+-- 
+-- > > diminished7th <&> ( • Note G Sharp 3 )
+-- > [G#3,B3,D4,F4]
+--
+-- > > minor11th <&> ( • Note D Natural 3 )
+-- > [D3,A3,E4,F4,C5,G5]
+
+-- ** Scales
+
+-- | Modes of C major.
+mode :: NoteName -> [ Interval ]
+mode root =
+  map
+    ( \ ( n, i ) -> Note root Natural 0 --> Note n Natural i )
+    ( map ( , 0 ) [ root .. maxBound ] ++ map ( , 1 ) [ minBound .. root ] )
+
+-- | Phrygian scale.
+phrygian :: [ Interval ]
+phrygian = mode E
+
+-- | Lydian scale.
+lydian :: [ Interval ]
+lydian = mode F
+
+-- | Whole tone scale.
+wholeTone :: [ Interval ]
+wholeTone = scanl (<>) mempty
+  [ Interval 2 Natural, Interval 2 Natural, Interval 2 Natural, Interval 3 DoubleFlat, Interval 2 Natural ]
+
+-- $scales
+-- Example scales:
+--
+-- > > phrygian <&> ( • Note E Natural 3 )
+-- > [E3,F3,G3,A3,B3,C4,D4,E4]
+--
+-- > > phrygian <&> ( • Note C Sharp 3 )
+-- > [C#3,D3,E3,F#3,G#3,A3,B3,C#4]
+--
+-- > > lydian <&> ( • Note C Natural 4 )
+-- > [C4,D4,E4,F#4,G4,A4,B4,C5]
+--
+-- > > wholeTone <&> ( • Note G Natural 5 )
+-- > [G5,A5,B5,C#6,Eb6,F6]
+
+---------------------------------------------------
+-- * Helper code
+-- $end
+-- End of main example code.
+--
+-- Follows: helper code for reading/showing musical notes and intervals.
+
+pattern Natural :: Alteration
+pattern Natural = Alteration 0
+pattern Flat :: Alteration
+pattern Flat = Alteration (-1)
+pattern DoubleFlat :: Alteration
+pattern DoubleFlat = Alteration (-2)
+pattern Sharp :: Alteration
+pattern Sharp = Alteration 1
+pattern DoubleSharp :: Alteration
+pattern DoubleSharp = Alteration 2
+
+instance Show Alteration where
+  show ( Alteration i ) = replicate ( abs i ) accidental
+    where
+      accidental :: Char
+      accidental
+        | i >= 0 = '#'
+        | otherwise = 'b'
+
+instance Show Note where
+  show ( Note n alt oct ) = show n <> show alt <> show oct
+
+pattern Interval :: Int -> Alteration -> Interval
+pattern Interval i a <-
+  ( ( \ ( Steps ( Sum steps ) alt ) -> ( if steps >= 0 then steps + 1 else steps - 1, alt ) )
+  -> (i, a)
+  )
+  where
+    Interval i a = if i < 0 then Steps ( Sum (i+1) ) a else Steps ( Sum (i-1) ) a
+
+instance Show Interval where
+  show ival@( Steps ( Sum i ) _ )
+    | i == 0 || i == 7 || i == -7
+    , let
+        ivalName = case compare i 0 of
+          LT -> "octave down"
+          GT -> "octave up"
+          EQ -> "unison"
+    = if quality ival == "perfect"
+      then ivalName
+      else quality ival <> " " <> ivalName
+    | i < 0
+    = quality ( inverse ival ) <> " " <> showOrdinal (-i+1) <> " down"
+    | otherwise
+    = quality ival <> " " <> showOrdinal (i+1) <> " up"
+
+quality :: Interval -> String
+quality ( Steps ( Sum i ) ( Alteration a ) )
+  | ( i `mod` 7 ) `elem` [ 0, 3, 4 ]
+  = case a of
+      0 -> "perfect"
+      _ -> 
+        if a > 0
+        then multiplicity   a  <> "augmented"
+        else multiplicity (-a) <> "diminished"
+  | otherwise
+  = case a of
+      0    -> "major"
+      (-1) -> "minor"
+      _    -> 
+        if a > 0
+        then multiplicity   a    <> "augmented"
+        else multiplicity (-a-1) <> "diminished"
+
+showOrdinal :: Int -> String
+showOrdinal i
+  | i < 0
+  = "-" <> showOrdinal ( abs i )
+  | i `mod` 10 == 1 && i `mod` 100 /= 11
+  = show i <> "st"
+  | i `mod` 10 == 2 && i `mod` 100 /= 12
+  = show i <> "nd"
+  | i `mod` 10 == 3 && i `mod` 100 /= 13
+  = show i <> "rd"
+  | otherwise
+  = show i <> "th"
+
+multiplicity :: Int -> String
+multiplicity 1 = ""
+multiplicity 2 = "doubly "
+multiplicity 3 = "triply "
+multiplicity 4 = "quadruply "
+multiplicity 5 = "quintuply "
+multiplicity 6 = "sextuply "
+multiplicity 7 = "heptuply "
+multiplicity n = show n <> "-tuply "
---
+++ src/Data/Act.hs
@@ -0,0 +1,210 @@
+{-# LANGUAGE
+    DeriveGeneric
+  , DeriveDataTypeable
+  , DerivingVia
+  , FlexibleContexts
+  , FlexibleInstances
+  , GeneralizedNewtypeDeriving
+  , MultiParamTypeClasses
+  , ScopedTypeVariables
+  , StandaloneDeriving
+  , UndecidableInstances
+#-}
+
+{-|
+Module: Data.Act
+
+An "Act" of a semigroup \( S \) on a type \( X \) gives a way to transform terms of type \( X \) by terms of type \( S \),
+in a way that is compatible with the semigroup operation on \( S \).
+
+In the special case that there is a unique way of going from one term of type \( X \) to another
+through a transformation by a term of type \( S \), we say that \( X \) is a torsor under \( S \).
+
+For example, the plane has an action by translations. Given any two points, there is a unique translation
+that takes the first point to the second. Note that an unmarked plane (like a blank piece of paper)
+has no designated origin or reference point, whereas the set of translations is a plane with a given origin
+(the zero translation). This is the distinction between an affine space (an unmarked plane) and a vector space.
+Enforcing this distinction in the types can help to avoid confusing absolute points with translation vectors.
+
+
+Simple 'Act' and 'Torsor' instances can be derived through self-actions:
+
+> > newtype Seconds   = Seconds { getSeconds :: Double }
+> >   deriving ( Act TimeDelta, Torsor TimeDelta )
+> >     via TimeDelta
+> > newtype TimeDelta = TimeDelta { timeDeltaInSeconds :: Seconds }
+> >   deriving ( Semigroup, Monoid, Group )
+> >     via Sum Double
+
+-}
+
+module Data.Act
+  ( Act(..)
+  , transportAction
+  , Trivial(..)
+  , Torsor(..)
+  , intertwiner
+  )
+  where
+
+-- base
+import Data.Coerce
+  ( coerce )
+import Data.Data
+  ( Data )
+import Data.Functor.Const
+  ( Const(..) )
+import Data.Functor.Contravariant
+  ( Op(..) )
+import Data.Monoid
+  ( Any(..), All(..)
+  , Sum(..), Product(..)
+  , Ap(..), Endo(..)
+  )
+import Data.Semigroup
+  ( Max(..), Min(..), Dual(..) )
+import GHC.Generics
+  ( Generic, Generic1 )
+
+-- deepseq
+import Control.DeepSeq
+  ( NFData )
+
+-- acts
+import Data.Group
+  ( Group(..) )
+
+-----------------------------------------------------------------
+
+-- | A left __act__ (or left __semigroup action__) of a semigroup @s@ on @x@ consists of an operation
+-- 
+-- @(•) :: s -> x -> x@
+--
+-- such that:
+-- 
+-- @a • ( b • x ) = ( a <> b ) • x@
+--
+-- In case @s@ is also a 'Monoid', we additionally require:
+--
+-- @mempty • x = x@
+--
+-- The synonym @ act = (•) @ is also provided.
+class Semigroup s => Act s x where
+  {-# MINIMAL (•) | act #-}
+  -- | Left action of a semigroup.
+  (•), act :: s -> x -> x
+  (•) = act
+  act = (•)
+
+infixr 5 •
+infixr 5 `act`
+
+-- | Transport an act:
+--
+-- <<img/transport.svg>>
+transportAction :: ( a -> b ) -> ( b -> a ) -> ( g -> b -> b ) -> ( g -> a -> a )
+transportAction to from actBy g = from . actBy g . to
+
+-- | Natural left action of a semigroup on itself.
+instance Semigroup s => Act s s where
+  (•) = (<>)
+
+-- | Trivial act of a semigroup on any type (acting by the identity).
+newtype Trivial a = Trivial { getTrivial :: a }
+  deriving stock   ( Show, Read, Data, Generic, Generic1 )
+  deriving newtype ( Eq, Ord, Enum, Bounded, NFData )
+instance Semigroup s => Act s ( Trivial a ) where
+  act _ = id
+
+deriving via Any instance Act Any Bool
+deriving via All instance Act All Bool
+deriving via ( Sum     a ) instance Num a => Act ( Sum     a ) a
+deriving via ( Product a ) instance Num a => Act ( Product a ) a
+deriving via ( Max     a ) instance Ord a => Act ( Max     a ) a
+deriving via ( Min     a ) instance Ord a => Act ( Min     a ) a
+
+instance {-# OVERLAPPING #-} Act () x where
+  act _ = id
+instance ( Act s1 x1, Act s2 x2 )
+      => Act ( s1, s2 ) ( x1,x2 ) where
+  act ( s1, s2 ) ( x1, x2 ) =
+    ( act s1 x1, act s2 x2 )
+instance ( Act s1 x1, Act s2 x2, Act s3 x3 )
+      => Act ( s1, s2, s3 ) ( x1, x2, x3 ) where
+  act ( s1, s2, s3 ) ( x1, x2, x3 ) =
+    ( act s1 x1, act s2 x2, act s3 x3 )
+instance ( Act s1 x1, Act s2 x2, Act s3 x3, Act s4 x4 )
+      => Act ( s1, s2, s3, s4 ) ( x1, x2, x3, x4 ) where
+  act ( s1, s2, s3, s4 ) ( x1, x2, x3, x4 ) =
+    ( act s1 x1, act s2 x2, act s3 x3, act s4 x4 )
+instance ( Act s1 x1, Act s2 x2, Act s3 x3, Act s4 x4, Act s5 x5 )
+      => Act ( s1, s2, s3, s4, s5 ) ( x1, x2, x3, x4, x5 ) where
+  act ( s1, s2, s3, s4, s5 ) ( x1, x2, x3, x4, x5 ) =
+    ( act s1 x1, act s2 x2, act s3 x3, act s4 x4, act s5 x5 )
+
+deriving newtype instance Act s a => Act s ( Const a b )
+
+-- | Acting through a functor using @fmap@.
+instance ( Act s x, Functor f ) => Act s ( Ap f x ) where
+  act s = coerce ( fmap ( act s ) :: f x -> f x )
+
+-- | Acting through the contravariant function arrow functor.
+instance ( Semigroup s, Act s a ) => Act ( Dual s ) ( Op b a ) where
+  act ( Dual s ) = coerce ( ( . act s ) :: ( a -> b ) -> ( a -> b ) )
+
+-- | Acting through a function arrow: both covariant and contravariant actions.
+instance ( Semigroup s, Act s a, Act t b ) => Act ( Dual s, t ) ( a -> b ) where
+  act ( Dual s, t ) p = act t . p . act s
+
+-- | Action of an opposite group using inverses.
+instance {-# OVERLAPPABLE #-} ( Act g x, Group g ) => Act ( Dual g ) x where
+  act ( Dual g ) = act ( inverse g )
+
+-- | Action of a group on endomorphisms.
+instance ( Group g, Act g a ) => Act g ( Endo a ) where
+  act g = coerce ( act ( Dual g, g ) :: ( a -> a ) -> ( a -> a ) )
+
+-----------------------------------------------------------------
+
+-- | A left __torsor__ consists of a /free/ and /transitive/ left action of a group on an inhabited type.
+--
+-- This precisely means that for any two terms @x@, @y@, there exists a /unique/ group element @g@ taking @x@ to @y@,
+-- which is denoted @ y <-- x @ (or @ x --> y @, but the left-pointing arrow is more natural when working with left actions).
+--
+-- That is @ y <-- x @ is the /unique/ element satisfying:
+--
+-- @( y <-- x ) • x = y@
+--
+--
+-- Note the order of composition of @<--@ and @-->@ with respect to @<>@:
+--
+-- > ( z <-- y ) <> ( y <-- x ) = z <-- x
+--
+-- > ( y --> z ) <> ( x --> y ) = x --> z
+class ( Group g, Act g x ) => Torsor g x where
+  {-# MINIMAL (-->) | (<--) #-}
+  -- | Unique group element effecting the given transition
+  (<--), (-->) :: x -> x -> g
+  (-->) = flip (<--)
+  (<--) = flip (-->)
+
+infix 7 -->
+infix 7 <--
+
+-- | Any group is a torsor under its own natural left action.
+instance Group g => Torsor g g where
+  h <-- g = h <> inverse g
+
+deriving via ( Sum a ) instance Num a => Torsor ( Sum a ) a
+
+-- | Given
+-- 
+--  * \( g \in G \) acting on \( A \),
+--  * \( B \) a torsor under \( H \),
+--  * a map \( p \colon A \to B \),
+--
+-- this function returns the unique element \( h \in H \) making the following diagram commute:
+--
+-- <<img/intertwiner.svg>>
+intertwiner :: forall h g a b. ( Act g a, Torsor h b ) => g -> ( a -> b ) -> a -> h
+intertwiner g p a = p a --> p ( g • a )
